c40bfa1b9d9af69b22487d446201dfd378a75e93
chowdhury-yeamin <chowdhuryyeamin07@gmail.com>
2025-11-23 15:59:25 +0600

    Started making Project

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-white min-h-screen text-black">
+       <h1 className="">Welcome to Tech Gadgets Shop</h1>
+    </div>
+  );
+}
